c905961f96be7e1804661fc1e3cb65826a0cb3ba
LeonardoSSm <deadsilva6@gmail.com>
2025-10-11 21:33:52 -0300

    add: vercel analytics

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,13 @@ import { createRoot } from 'react-dom/client'
 import { AppLayout } from './app/layout'
 import App from './app/App'
 import './shared/styles/globals.css'
+import { Analytics } from '@vercel/analytics/react'
 
 createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <AppLayout>
       <App />
+      <Analytics />
     </AppLayout>
   </React.StrictMode>
 )
